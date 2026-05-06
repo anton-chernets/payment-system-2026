f973f0046d4e3a43b38dff8742ed74aa293f3db3
anton.chernets <anton.chernets@artpix3d.com>
2026-05-06 14:03:43 +0300

    Add Swagger (l5-swagger) with health check endpoint

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use OpenApi\Attributes as OA;
+
+#[OA\Info(title: 'Test Payment System API', version: '1.0.0')]
+#[OA\Server(url: '/api', description: 'API Server')]
 abstract class Controller
 {
-    //
 }
